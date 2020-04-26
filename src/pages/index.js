7db3ad5d899a0c216af06f86ae0362da352fe526
taiga <taiga.similarities@gmail.com>
2020-04-27 00:47:53 +0900

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link,graphql } from "gatsby";
+
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import Info from "../components/Home/Info";
+import BackgroundSection from "../components/Globals/BackgroundSection";
+
+const IndexPage = ({data}) => (
+  <Layout>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+  <BackgroundSection
+   img={data.img.childImageSharp.fluid}
+   title="regular joe's " 
+  />
+  <Info />
+  </Layout>
+);
+
+
+export const query =  graphql`
+{
+  img:file(relativePath:{eq:
+    "default-background.jpeg"}) {
+    childImageSharp{
+      fluid{
+        ...GatsbyImageSharpFluid_tracedSVG
+      }
+    }
+  }
+}
+`;
+
+export default IndexPage;
