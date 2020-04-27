aa4e9162577c1b0927b92d4434c031aa1d610309
taiga <taiga.similarities@gmail.com>
2020-04-28 02:44:46 +0900

    Complete contentful picture

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,8 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+
 module.exports = {
   siteMetadata: {
     title: `Regular Joe's`,
@@ -12,6 +17,14 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images`
       }
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId:process.env.CONTENTFUL_SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { Link,graphql } from "gatsby";
+import { Link, graphql } from "gatsby";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import Info from "../components/Home/Info";
+import Menu from "../components/Home/Menu";
+
 import BackgroundSection from "../components/Globals/BackgroundSection";
 
 const IndexPage = ({data}) => (
@@ -14,17 +16,39 @@ const IndexPage = ({data}) => (
    title="regular joe's " 
   />
   <Info />
+  <Menu items={data.menu}/>
   </Layout>
 );
 
 
+
+
 export const query =  graphql`
+
 {
   img:file(relativePath:{eq:
     "default-background.jpeg"}) {
     childImageSharp{
       fluid{
         ...GatsbyImageSharpFluid_tracedSVG
+      }
+    }
+  }
+  menu: allContentfulCoffeeItem {
+    edges{
+      node{
+        id
+        title
+        description{
+          description
+        }
+        price
+        category
+        image {
+          fixed(width:50,height:50){
+            ...GatsbyContentfulFixed_tracedSVG
+          }
+        }
       }
     }
   }
