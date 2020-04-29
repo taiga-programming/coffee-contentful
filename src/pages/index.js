dbee8217fa229461506bec5cabb4975f6acf9ef2
taiga <taiga.similarities@gmail.com>
2020-04-29 17:16:21 +0900

    added snipcart

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,8 @@ import Layout from "../components/layout";
 import SEO from "../components/seo";
 import Info from "../components/Home/Info";
 import Menu from "../components/Home/Menu";
+import Products from "../components/Home/Products";
+import Contact from "../components/Home/Contact";
 
 import BackgroundSection from "../components/Globals/BackgroundSection";
 
@@ -13,10 +15,12 @@ const IndexPage = ({data}) => (
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
   <BackgroundSection
    img={data.img.childImageSharp.fluid}
-   title="regular joe's " 
+   title="A CUP OF COFFEE " 
   />
   <Info />
   <Menu items={data.menu}/>
+  <Products/>
+  <Contact/>
   </Layout>
 );
 
@@ -27,7 +31,7 @@ export const query =  graphql`
 
 {
   img:file(relativePath:{eq:
-    "default-background.jpeg"}) {
+    "jakub-kapusnak-1LaCrYx5J7I-unsplash.jpg"}) {
     childImageSharp{
       fluid{
         ...GatsbyImageSharpFluid_tracedSVG
